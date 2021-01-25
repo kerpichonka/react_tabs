20e893214a3918e71ed0edb7d19923e4030cb09b
Dima <dimontregyb@email.ua>
2021-01-25 20:41:29 +0200

    Updates

---
+++ precache-manifest.02e8c2ecb15f85cc549501a8985bb68b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e6a77abb57616952d38ee112e3a6b98",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4da57017bedb896e9450",
+    "url": "./static/css/main.909b8524.chunk.css"
+  },
+  {
+    "revision": "4b964fc25ecc0b658447",
+    "url": "./static/js/2.18166693.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.18166693.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4da57017bedb896e9450",
+    "url": "./static/js/main.e5c7e288.chunk.js"
+  },
+  {
+    "revision": "53d1114eedfc0eaca354",
+    "url": "./static/js/runtime-main.a6df9e19.js"
+  }
+]);
